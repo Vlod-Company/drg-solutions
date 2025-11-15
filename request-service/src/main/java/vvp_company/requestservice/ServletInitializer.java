6192c5ce2f8787bebed35280dc2f3085aabc009c
Vainslav <Vladik_ww@mail.ru>
2025-11-15 14:21:37 +0300

    Request-service. init

---
+++ request-service/src/main/java/vvp_company/requestservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package vvp_company.requestservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RequestServiceApplication.class);
+    }
+
+}
---
+++ request-service/src/test/java/vvp_company/requestservice/RequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vvp_company.requestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
