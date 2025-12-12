3c4a70730fefaa4dc8c507d22e958b14f5d58f4f
Vainslav <Vladik_ww@mail.ru>
2025-12-12 21:38:10 +0300

    Service-registry. ISSUE-37. Сделял

---
+++ service-registry/src/main/java/vvp_company/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,22 @@
+package vvp_company.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServiceRegistryApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
