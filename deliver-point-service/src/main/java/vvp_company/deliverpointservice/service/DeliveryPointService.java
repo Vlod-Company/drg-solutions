00caf5e9f2cb877df0b8823b4fb299a9d83d9fa5
Vainslav <Vladik_ww@mail.ru>
2025-12-14 12:23:50 +0300

    Delivery-point-service. ISSUE-0. Поменял тип транзакций

--- deliver-point-service/src/main/java/vvp_company/deliverpointservice/service/DeliveryPointService.java
+++ deliver-point-service/src/main/java/vvp_company/deliverpointservice/service/DeliveryPointService.java
@@ -3,6 +3,8 @@ package vvp_company.deliverpointservice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import vvp_company.deliverpointservice.enm.DeliveryPointType;
 import vvp_company.deliverpointservice.model.DeliveryPoint;
@@ -10,12 +12,15 @@ import vvp_company.deliverpointservice.repository.DeliveryPointRepository;
 
 import java.util.List;
 
+import static org.springframework.transaction.annotation.Propagation.MANDATORY;
+
 @Service
 @RequiredArgsConstructor
 public class DeliveryPointService {
 
     private final DeliveryPointRepository deliveryPointRepository;
 
+    @Transactional(propagation = MANDATORY)
     public DeliveryPoint createDeliveryPoint(DeliveryPointType deliveryPointType) {
         var deliveryPoint = DeliveryPoint.builder().deliveryType(deliveryPointType).build();
         return deliveryPointRepository.save(deliveryPoint);
@@ -25,6 +30,7 @@ public class DeliveryPointService {
         return deliveryPointRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @Transactional(propagation = MANDATORY)
     public void removeDeliveryPoint(Long id) {
         deliveryPointRepository.deleteById(id);
     }
