b528f70f658c3e16737bc439802279d25c54f2cc
sashka <bogoyavlenscysanya@gmail.com>
2025-12-20 13:37:23 +0300

    Logistics-service. ISSUE-39. Done

--- logistics-service/src/main/java/vvp_company/logisticsservice/LogisticsServiceApplication.java
+++ logistics-service/src/main/java/vvp_company/logisticsservice/LogisticsServiceApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import vvp_company.logisticsservice.config.FeignConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = FeignConfig.class)
 public class LogisticsServiceApplication {
 
 	public static void main(String[] args) {
--- logistics-service/src/main/java/vvp_company/logisticsservice/controller/LogisticController.java
+++ logistics-service/src/main/java/vvp_company/logisticsservice/controller/LogisticController.java
@@ -2,6 +2,7 @@ package vvp_company.logisticsservice.controller;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import vvp_company.logisticsservice.enm.LogisticStatus;
 import vvp_company.logisticsservice.model.Logistic;
@@ -16,6 +17,8 @@ public class LogisticController {
 
     private final LogisticService logisticService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_LAUNCH_CONTROL_EMPLOYEE')")
     @PutMapping("{id}")
     public Logistic updateLogisticStatusAndDate(@PathVariable Long id, @NotNull @RequestParam("newStatus") LogisticStatus newStatus, @RequestParam("newDate") LocalDateTime newDate) {
         return logisticService.updateLogistic(id, newStatus, newDate);
