3a9312ba7ba7f37829c9a0545deca7d1a7c2ce87
Vainslav <Vladik_ww@mail.ru>
2025-12-20 13:43:20 +0300

    Delivery-point-service. ISSUE-0. Сделал

--- deliver-point-service/src/main/java/vvp_company/deliverpointservice/DeliverPointServiceApplication.java
+++ deliver-point-service/src/main/java/vvp_company/deliverpointservice/DeliverPointServiceApplication.java
@@ -2,9 +2,16 @@ package vvp_company.deliverpointservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeliverPointServiceApplication {
+public class DeliverPointServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DeliverPointServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeliverPointServiceApplication.class, args);
