573bec9eafc37bcbbdfa87ee88c5bdee59714e85
Vainslav <Vladik_ww@mail.ru>
2025-12-20 15:07:35 +0300

    Station-service. ISSUE-0. Поправил

--- station-service/src/main/java/vvp_company/stationservice/StationServiceApplication.java
+++ station-service/src/main/java/vvp_company/stationservice/StationServiceApplication.java
@@ -2,6 +2,8 @@ package vvp_company.stationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import vvp_company.stationservice.config.FeignConfig;
@@ -9,7 +11,12 @@ import vvp_company.stationservice.config.FeignConfig;
 @SpringBootApplication
 @EnableFeignClients(defaultConfiguration = FeignConfig.class)
 @EnableDiscoveryClient
-public class StationServiceApplication {
+public class StationServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StationServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StationServiceApplication.class, args);
