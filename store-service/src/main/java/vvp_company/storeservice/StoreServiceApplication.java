52ed5fb1bd8830f0d608fcc4b8a33d90aa1931ba
Vainslav <Vladik_ww@mail.ru>
2025-12-20 15:10:23 +0300

    Store-service. ISSUE-0. Поправил

--- store-service/src/main/java/vvp_company/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/vvp_company/storeservice/StoreServiceApplication.java
@@ -2,6 +2,8 @@ package vvp_company.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import vvp_company.storeservice.config.FeignConfig;
@@ -9,7 +11,12 @@ import vvp_company.storeservice.config.FeignConfig;
 @SpringBootApplication
 @EnableFeignClients(defaultConfiguration = FeignConfig.class)
 @EnableDiscoveryClient
-public class StoreServiceApplication {
+public class StoreServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StoreServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StoreServiceApplication.class, args);
