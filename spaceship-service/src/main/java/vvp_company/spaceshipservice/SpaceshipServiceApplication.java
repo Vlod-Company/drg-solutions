a1db7b2e48137b1841f775806bbd8d3de1f7f83b
Vainslav <Vladik_ww@mail.ru>
2025-12-20 15:04:52 +0300

    Spaceship-service. ISSUE-0. Добавил

--- spaceship-service/src/main/java/vvp_company/spaceshipservice/SpaceshipServiceApplication.java
+++ spaceship-service/src/main/java/vvp_company/spaceshipservice/SpaceshipServiceApplication.java
@@ -2,11 +2,18 @@ package vvp_company.spaceshipservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpaceshipServiceApplication {
+public class SpaceshipServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpaceshipServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaceshipServiceApplication.class, args);
