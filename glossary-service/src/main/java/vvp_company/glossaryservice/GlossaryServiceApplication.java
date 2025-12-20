bff111522e32408537eb02b06028cd9db9658fe0
Vainslav <Vladik_ww@mail.ru>
2025-12-20 13:31:54 +0300

    Glossary-service. ISSUE-0. Поменял packaging, добавил дискавери

--- glossary-service/src/main/java/vvp_company/glossaryservice/GlossaryServiceApplication.java
+++ glossary-service/src/main/java/vvp_company/glossaryservice/GlossaryServiceApplication.java
@@ -2,9 +2,18 @@ package vvp_company.glossaryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class GlossaryServiceApplication {
+@EnableDiscoveryClient
+public class GlossaryServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GlossaryServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GlossaryServiceApplication.class, args);
