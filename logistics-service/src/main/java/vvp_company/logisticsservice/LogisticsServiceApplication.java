b1979e7d2542d98295decfd03a94363bc40e7e19
Vainslav <Vladik_ww@mail.ru>
2025-12-20 14:11:09 +0300

    Logistics-service. ISSUE-0. ТЕПЕРЬ ТОЧНО ВСЁ РАБОТЕТ

--- logistics-service/src/main/java/vvp_company/logisticsservice/LogisticsServiceApplication.java
+++ logistics-service/src/main/java/vvp_company/logisticsservice/LogisticsServiceApplication.java
@@ -2,6 +2,8 @@ package vvp_company.logisticsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -10,7 +12,12 @@ import vvp_company.logisticsservice.config.FeignConfig;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(defaultConfiguration = FeignConfig.class)
-public class LogisticsServiceApplication {
+public class LogisticsServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LogisticsServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticsServiceApplication.class, args);
