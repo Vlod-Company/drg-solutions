0a90592c522f078a7826eb09cfdd7bc23596d800
Vainslav <Vladik_ww@mail.ru>
2025-12-20 13:46:44 +0300

    Employee-service. ISSUE-0. Поменял packaging

--- employee-service/src/main/java/vvp_company/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/vvp_company/employeeservice/EmployeeServiceApplication.java
@@ -2,11 +2,18 @@ package vvp_company.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EmployeeServiceApplication {
+public class EmployeeServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
