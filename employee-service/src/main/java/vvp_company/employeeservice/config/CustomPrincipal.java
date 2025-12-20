c85fdda14bc106269a09e044834bd43a1d74ebd3
sashka <bogoyavlenscysanya@gmail.com>
2025-12-20 12:59:55 +0300

    Employee-service. ISSUE-39. Done

---
+++ employee-service/src/main/java/vvp_company/employeeservice/config/CustomPrincipal.java
@@ -0,0 +1,11 @@
+package vvp_company.employeeservice.config;
+
+public class CustomPrincipal {
+    public final Long employeeId;
+    public final String department;
+
+    public CustomPrincipal(String employeeId, String department) {
+        this.employeeId = Long.parseLong(employeeId);
+        this.department = department;
+    }
+}
