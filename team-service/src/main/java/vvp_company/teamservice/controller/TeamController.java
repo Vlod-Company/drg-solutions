1b6eb1b73064174ab2043f9d7501afe722146a9a
sashka <bogoyavlenscysanya@gmail.com>
2025-12-20 14:55:20 +0300

    Team-service. ISSUE-39. Done

--- team-service/src/main/java/vvp_company/teamservice/controller/TeamController.java
+++ team-service/src/main/java/vvp_company/teamservice/controller/TeamController.java
@@ -2,6 +2,7 @@ package vvp_company.teamservice.controller;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import vvp_company.teamservice.dto.TeamDto;
 import vvp_company.teamservice.enm.TeamStatus;
@@ -16,6 +17,9 @@ public class TeamController {
 
     private final TeamService teamService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_LAUNCH_CONTROL_EMPLOYEE') "+
+            "or hasRole('ROLE_MISSION_CONTROL_EMPLOYEE')")
     @PostMapping
     public TeamDto createTeam(@NotBlank @RequestParam String teamName) {
         return teamService.createTeam(teamName);
@@ -31,11 +35,17 @@ public class TeamController {
         return teamService.findTeam(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_LAUNCH_CONTROL_EMPLOYEE') "+
+            "or hasRole('ROLE_MISSION_CONTROL_EMPLOYEE')")
     @DeleteMapping("/{id}")
     public void deleteTeam(@PathVariable Long id) {
         teamService.deleteTeam(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_LAUNCH_CONTROL_EMPLOYEE') "+
+            "or hasRole('ROLE_MISSION_CONTROL_EMPLOYEE')")
     @PutMapping("/{id}")
     public void updateTeam(@PathVariable Long id, @NotBlank @RequestParam("status") TeamStatus status, @RequestParam("cargoId") Long cargoId) {
         teamService.updateTeamStatus(id, status, cargoId);
--- team-service/src/main/java/vvp_company/teamservice/dto/TeamDto.java
+++ team-service/src/main/java/vvp_company/teamservice/dto/TeamDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import vvp_company.missionservice.enm.TeamStatus;
+import vvp_company.teamservice.enm.TeamStatus;
 
 @Data
 @Builder
