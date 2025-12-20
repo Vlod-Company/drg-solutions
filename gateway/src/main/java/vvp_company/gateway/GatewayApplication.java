b738b12cd91218fa917609b0ed683c2765b7542c
Vainslav <Vladik_ww@mail.ru>
2025-12-20 14:02:59 +0300

    Gateway-service. ISSUE-0. ВОТ ТЕПЕРЬ ВСЁ ТОЧНО РАБОТАЕТ

--- gateway/src/main/java/vvp_company/gateway/GatewayApplication.java
+++ gateway/src/main/java/vvp_company/gateway/GatewayApplication.java
@@ -2,13 +2,20 @@ package vvp_company.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class GatewayApplication {
+public class GatewayApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GatewayApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
