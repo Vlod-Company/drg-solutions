5eb674f2439db7b0eab35cddd3b775b341a3d51b
sashka <bogoyavlenscysanya@gmail.com>
2025-12-20 13:13:20 +0300

    Delivery-point-service. ISSUE-39. Add roles

--- deliver-point-service/src/main/java/vvp_company/deliverpointservice/controller/DeliveryPointController.java
+++ deliver-point-service/src/main/java/vvp_company/deliverpointservice/controller/DeliveryPointController.java
@@ -1,6 +1,7 @@
 package vvp_company.deliverpointservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import vvp_company.deliverpointservice.enm.DeliveryPointType;
 import vvp_company.deliverpointservice.model.DeliveryPoint;
@@ -15,11 +16,25 @@ public class DeliveryPointController {
 
     private final DeliveryPointService deliveryPointService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_MAINTENANCE_EMPLOYEE') " +
+            "or hasRole('ROLE_LAUNCH_CONTROL_EMPLOYEE') " +
+            "or hasRole('ROLE_MISSION_CONTROL_EMPLOYEE') " +
+            "or hasRole('ROLE_MANAGEMENT_EMPLOYEE') " +
+            "or hasRole('ROLE_SCIENCE_DEPARTMENT_EMPLOYEE') " +
+            "or hasRole('ROLE_SCANCOM_EMPLOYEE')")
     @GetMapping("{id}")
     public DeliveryPoint getDeliveryPoint(@PathVariable("id") Long id) {
         return deliveryPointService.findById(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_MAINTENANCE_EMPLOYEE') " +
+            "or hasRole('ROLE_LAUNCH_CONTROL_EMPLOYEE') " +
+            "or hasRole('ROLE_MISSION_CONTROL_EMPLOYEE') " +
+            "or hasRole('ROLE_MANAGEMENT_EMPLOYEE') " +
+            "or hasRole('ROLE_SCIENCE_DEPARTMENT_EMPLOYEE') " +
+            "or hasRole('ROLE_SCANCOM_EMPLOYEE')")
     @GetMapping
     public List<DeliveryPoint> getAllDeliveryPoints() {
         return deliveryPointService.findAll();
